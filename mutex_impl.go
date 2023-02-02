db585bf6ceeeb6fbd697c99839b7ecdf16a922de
tgnike <43302514+tgnike@users.noreply.github.com>
2023-02-02 22:21:50 +0300

    Update mutex_impl.go

--- mutex_impl.go
+++ mutex_impl.go
@@ -13,17 +13,24 @@ type MuContest struct {
 func (mu *MuContest) Lock() {
 
 	// Увеличение количества блокировок
+	// если получилось больше 1 - есть еще блокировки
+	// ожидаем сингнал из канала (ch)
 	if atomic.AddInt32(&mu.nlocks, 1) == 1 {
 		return
 	}
 
+	// Ожидание сигнала из Unlock()
 	<-mu.ch
 
 }
 
 func (mu *MuContest) LockChannel() <-chan struct{} {
 
-	if atomic.LoadInt32(&mu.nlocks) == 0 {
+	// if atomic.LoadInt32(&mu.nlocks) == 0 {
+	// 	mu.ch <- mu.sig
+	// }
+
+	if atomic.AddInt32(&mu.nlocks, 1) == 1 {
 		mu.ch <- mu.sig
 	}
 
@@ -33,11 +40,17 @@ func (mu *MuContest) LockChannel() <-chan struct{} {
 
 func (mu *MuContest) Unlock() {
 
+	// Уменьшение счетчика блокировок
+	// если есть еще блокировки отправляем сигнал в канал (ch).
 	if atomic.AddInt32(&mu.nlocks, -1) == 0 {
 		return
 	}
 
-	// Если не все блокировки сняты, отправляем сингнал о разблокировке
+	if atomic.LoadInt32(&mu.nlocks) < 0 {
+		panic("unlocked")
+	}
+
+	// Сигнал о разблокировке
 	mu.ch <- mu.sig
 
 }
